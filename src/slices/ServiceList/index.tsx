a81483083d30ae2201fbfc5f6eedc114801ff25d
Ochoagram <ricardogochoa@me.com>
2024-06-02 15:41:00 -0600

    testimonials

--- src/slices/ServiceList/index.tsx
+++ src/slices/ServiceList/index.tsx
@@ -6,7 +6,9 @@ import { Accordion, AccordionDetails, AccordionSummary, Divider } from "@mui/mat
 import AddIcon from '@mui/icons-material/Add';
 import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 import Typography from '@mui/material/Typography';
-
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 /**
  * Props for `ServiceList`.
@@ -31,7 +33,7 @@ const ServiceList = ({ slice }: ServiceListProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="max-w-1200" style={{ maxWidth: '1300px', margin: '0 auto', paddingLeft: '15px', paddingRight: '15px' }}>
+      <div className="max-w-1200" style={{ maxWidth: '1300px', margin: '0 auto', paddingLeft: '15px', paddingRight: '15px', marginBottom: '5rem' }}>
 
         {services.map((service, index) => (
           <Accordion
@@ -54,6 +56,59 @@ const ServiceList = ({ slice }: ServiceListProps): JSX.Element => {
         ))}
 
       </div>
+
+      <Carousel
+        showArrows={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={6100}
+      >
+        <div>
+          {/* <img src="/images/shirley.png" /> */}
+          <div className="myCarousel">
+            <h3>Maggye Montez</h3>
+            <h4>Fundadora y directora de operaciones de Taquería El Chilanguito</h4>
+            <p>
+              Un equipo muy metódico,  disciplinado y responsable con todo lo propuesto en cada proyecto desde el branding hasta el seguimiento de los clientes en caso de ser necesario.  Solución de problemas en redes. Más de 1 año siendo parte del nuestro proyecto.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          {/* <img src="/images/daniel.png" /> */}
+          <div className="myCarousel">
+            <h3>Carolina Cortes-Monroy</h3>
+            <h4>Agencia de Viajes de Boca en Boca Cancún</h4>
+            <p>
+              A lo largo del camino he conocido muchas empresas de diseño, redes y Marketing y ninguna había entendido el concepto que quería para mi agencia de viajes. Happiier logró plasmar la imagen de mi marca, comprendieron todas mis ideas y me ayudaron a organizarlas para llegar al mejor logotipo que pudiera tener.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          {/* <img src="/images/theo.png" /> */}
+          <div className="myCarousel">
+            <h3>Manu Navarro</h3>
+            <h4>Social Media Manager | Grupo Posadas</h4>
+            <p>
+              Durante más de 8 años, hemos tenido el placer de colaborar con Happiier Studio, una agencia de redes sociales que destaca por su creatividad y responsabilidad excepcionales. Su habilidad para manejar marcas de renombre como Fiesta Americana y The Explorean ha demostrado su capacidad para generar ideas innovadoras y resultados impresionantes. Su compromiso y originalidad son inigualables en la industria.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          {/* <img src="/images/theo.png" /> */}
+          <div className="myCarousel">
+            <h3>Jessica Muñoz</h3>
+            <h4>Fundadora | Socio Naiz </h4>
+            <p>
+              Trabajar con Happiier en medio de la llegada de nuestro hijo fue un gran alivio. Fué como confiarle a nuestro primer bebé, para poder enfocarnos en el que estaba por llegar. Su capacidad, entrega, disciplina, proactividad nos hizo confiar ciegamente y sentir que teníamos a un equipo mientras estábamos ausentes. Gracias por su entrega, compromiso y gran trabajo.
+            </p>
+          </div>
+        </div>
+      </Carousel>
 
     </section>
   );
